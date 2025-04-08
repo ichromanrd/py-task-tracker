e88bb5f86ba8791f4154d138ca67511bde79dc3b
ichromanrd <ichromanraditya.duwila@gmail.com>
2025-04-08 20:18:46 +0700

    task-tracker: add initial task entity

---
+++ task.py
@@ -0,0 +1,7 @@
+class Task():
+    def __init__(self, id, description, status, createdAt, updatedAt):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
